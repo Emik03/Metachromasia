93d2dd83f888b0a1c4aa09bba69ddd9ff0a5cc26
Emik <emik.cuber@gmail.com>
2026-04-03 20:53:07 +0200

    Document Signatures and Tag

--- Source/Signatures.cs
+++ Source/Signatures.cs
@@ -1,35 +1,106 @@
 // SPDX-License-Identifier: MPL-2.0
 namespace Metachromasia;
 
+/// <summary>Contains a bunch of delegates containing signatures used for patches.</summary>
+/// <remarks><para>
+/// This type doesn't cover up to the same amount of parameters as <see cref="Action"/>
+/// or <see cref="Func{TResult}"/>, but will cover the majority of signature use cases.
+/// </para></remarks>
 public static class Signatures // ReSharper disable InconsistentNaming
 {
+    /// <summary>Patch for <see cref="Action"/>.</summary>
+    /// <typeparam name="T">The type of instance, or <see cref="ValueTuple"/> for static methods.</typeparam>
     public delegate void ActPatch<in T>(T __instance);
 
+    /// <summary>Patch for <see cref="Action"/> in prefixes that can skip execution.</summary>
+    /// <typeparam name="T">The type of instance, or <see cref="ValueTuple"/> for static methods.</typeparam>
     public delegate bool PredPatch<in T>(T __instance);
 
+    /// <summary>Patch for <see cref="Action{T}"/>.</summary>
+    /// <typeparam name="T">The type of instance, or <see cref="ValueTuple"/> for static methods.</typeparam>
+    /// <typeparam name="T0">The type of the first parameter.</typeparam>
     public delegate void ActPatch<in T, T0>(T __instance, ref T0 __0);
 
+    /// <summary>Patch for <see cref="Action{T}"/> in prefixes that can skip execution.</summary>
+    /// <typeparam name="T">The type of instance, or <see cref="ValueTuple"/> for static methods.</typeparam>
+    /// <typeparam name="T0">The type of the first parameter.</typeparam>
     public delegate bool PredPatch<in T, T0>(T __instance, ref T0 __0);
 
+    /// <summary>Patch for <see cref="Action{T1, T2}"/>.</summary>
+    /// <typeparam name="T">The type of instance, or <see cref="ValueTuple"/> for static methods.</typeparam>
+    /// <typeparam name="T0">The type of the first parameter.</typeparam>
+    /// <typeparam name="T1">The type of the second parameter.</typeparam>
     public delegate void ActPatch<in T, T0, T1>(T __instance, ref T0 __0, ref T1 __1);
 
+    /// <summary>Patch for <see cref="Action{T1, T2}"/> in prefixes that can skip execution.</summary>
+    /// <typeparam name="T">The type of instance, or <see cref="ValueTuple"/> for static methods.</typeparam>
+    /// <typeparam name="T0">The type of the first parameter.</typeparam>
+    /// <typeparam name="T1">The type of the second parameter.</typeparam>
     public delegate bool PredPatch<in T, T0, T1>(T __instance, ref T0 __0, ref T1 __1);
 
+    /// <summary>Patch for <see cref="Action{T1, T2, T3}"/>.</summary>
+    /// <typeparam name="T">The type of instance, or <see cref="ValueTuple"/> for static methods.</typeparam>
+    /// <typeparam name="T0">The type of the first parameter.</typeparam>
+    /// <typeparam name="T1">The type of the second parameter.</typeparam>
+    /// <typeparam name="T2">The type of the third parameter.</typeparam>
     public delegate void ActPatch<in T, T0, T1, T2>(T __instance, ref T0 __0, ref T1 __1, ref T2 __2);
 
+    /// <summary>Patch for <see cref="Action{T1, T2, T3}"/> in prefixes that can skip execution.</summary>
+    /// <typeparam name="T">The type of instance, or <see cref="ValueTuple"/> for static methods.</typeparam>
+    /// <typeparam name="T0">The type of the first parameter.</typeparam>
+    /// <typeparam name="T1">The type of the second parameter.</typeparam>
+    /// <typeparam name="T2">The type of the third parameter.</typeparam>
     public delegate bool PredPatch<in T, T0, T1, T2>(T __instance, ref T0 __0, ref T1 __1, ref T2 __2);
 
+    /// <summary>Patch for <see cref="Action{T1, T2, T3, T4}"/>.</summary>
+    /// <typeparam name="T">The type of instance, or <see cref="ValueTuple"/> for static methods.</typeparam>
+    /// <typeparam name="T0">The type of the first parameter.</typeparam>
+    /// <typeparam name="T1">The type of the second parameter.</typeparam>
+    /// <typeparam name="T2">The type of the third parameter.</typeparam>
+    /// <typeparam name="T3">The type of the fourth parameter.</typeparam>
     public delegate void ActPatch<in T, T0, T1, T2, T3>(T __instance, ref T0 __0, ref T1 __1, ref T2 __2, ref T3 __3);
 
+    /// <summary>Patch for <see cref="Action{T1, T2, T3, T4}"/> in prefixes that can skip execution.</summary>
+    /// <typeparam name="T">The type of instance, or <see cref="ValueTuple"/> for static methods.</typeparam>
+    /// <typeparam name="T0">The type of the first parameter.</typeparam>
+    /// <typeparam name="T1">The type of the second parameter.</typeparam>
+    /// <typeparam name="T2">The type of the third parameter.</typeparam>
+    /// <typeparam name="T3">The type of the fourth parameter.</typeparam>
     public delegate bool PredPatch<in T, T0, T1, T2, T3>(T __instance, ref T0 __0, ref T1 __1, ref T2 __2, ref T3 __3);
 
+    /// <summary>Patch for <see cref="Func{TResult}"/>.</summary>
+    /// <typeparam name="T">The type of instance, or <see cref="ValueTuple"/> for static methods.</typeparam>
+    /// <typeparam name="TR">The type of non-void return value.</typeparam>
     public delegate void ActWithRetPatch<in T, TR>(T __instance, ref TR __result);
 
+    /// <summary>Patch for <see cref="Func{TResult}"/> in prefixes that can skip execution.</summary>
+    /// <typeparam name="T">The type of instance, or <see cref="ValueTuple"/> for static methods.</typeparam>
+    /// <typeparam name="TR">The type of non-void return value.</typeparam>
     public delegate bool PredWithRetPatch<in T, TR>(T __instance, ref TR __result);
 
+    /// <summary>Patch for <see cref="Func{T, TResult}"/>.</summary>
+    /// <typeparam name="T">The type of instance, or <see cref="ValueTuple"/> for static methods.</typeparam>
+    /// <typeparam name="TR">The type of non-void return value.</typeparam>
+    /// <typeparam name="T0">The type of the first parameter.</typeparam>
     public delegate void ActWithRetPatch<in T, TR, T0>(T __instance, ref TR __result, ref T0 __0);
 
+    /// <summary>Patch for <see cref="Func{T, TResult}"/> in prefixes that can skip execution.</summary>
+    /// <typeparam name="T">The type of instance, or <see cref="ValueTuple"/> for static methods.</typeparam>
+    /// <typeparam name="TR">The type of non-void return value.</typeparam>
+    /// <typeparam name="T0">The type of the first parameter.</typeparam>
+    public delegate void PredWithRetPatch<in T, TR, T0>(T __instance, ref TR __result, ref T0 __0);
+
+    /// <summary>Patch for <see cref="Func{T1, T2, TResult}"/>.</summary>
+    /// <typeparam name="T">The type of instance, or <see cref="ValueTuple"/> for static methods.</typeparam>
+    /// <typeparam name="TR">The type of non-void return value.</typeparam>
+    /// <typeparam name="T0">The type of the first parameter.</typeparam>
+    /// <typeparam name="T1">The type of the second parameter.</typeparam>
     public delegate void ActWithRetPatch<in T, TR, T0, T1>(T __instance, ref TR __result, ref T0 __0, ref T1 __1);
 
+    /// <summary>Patch for <see cref="Func{T1, T2, TResult}"/> in prefixes that can skip execution.</summary>
+    /// <typeparam name="T">The type of instance, or <see cref="ValueTuple"/> for static methods.</typeparam>
+    /// <typeparam name="TR">The type of non-void return value.</typeparam>
+    /// <typeparam name="T0">The type of the first parameter.</typeparam>
+    /// <typeparam name="T1">The type of the second parameter.</typeparam>
     public delegate bool PredWithRetPatch<in T, TR, T0, T1>(T __instance, ref TR __result, ref T0 __0, ref T1 __1);
 }
--- Source/Tag.cs
+++ Source/Tag.cs
@@ -1,45 +1,104 @@
 // SPDX-License-Identifier: MPL-2.0
 namespace Metachromasia;
 
+/// <summary>Represents the set of attributes that a <see cref="Plant"/> can possess.</summary>
 [Flags]
 public enum Tag
 {
+    /// <summary>No tag.</summary>
     None,
 
     /// <summary>This value is reserved for internal purposes and is not meant to be used outside of it.</summary>
     [EditorBrowsable(EditorBrowsableState.Never)]
     Reserved,
 
+    /// <summary>Whether the plant is an ultimate, limiting its use in Odyssey levels.</summary>
     Ulti,
+
+    /// <summary>Whether the plant is related to the snow levels.</summary>
     Snow = 1 << 2,
+
+    /// <summary>Whether the plant is incapable of being crushed.</summary>
     AntiCrush = 1 << 3,
+
+    /// <summary>Whether the plant is a giant nut that rolls across the field.</summary>
     BigNut = 1 << 4,
+
+    /// <summary>Whether the plant is flying.</summary>
     Fly = 1 << 8,
+
+    /// <summary>Whether the plant is in hard land.</summary>
     HardLand = 1 << 9,
+
+    /// <summary>Whether the plant can only be placed on water.</summary>
     Water = 1 << 10,
+
+    /// <summary>Whether the plant is a pumpkin, which can be placed on top of other plants.</summary>
     Pumpkin = 1 << 11,
+
+    /// <summary>Whether the plant is illuminated.</summary>
     Lantern = 1 << 12,
+
+    /// <summary>Whether the plant is a small lantern.</summary>
     SmallLantern = 1 << 13,
+
+    /// <summary>Whether the plant is a puff-shroom.</summary>
     Puff = 1 << 14,
+
+    /// <summary>Whether the plant is a nut.</summary>
     Nut = 1 << 15,
+
+    /// <summary>Whether the plant is a tall nut.</summary>
     TallNut = 1 << 16,
+
+    /// <summary>Whether the plant is a potato mine.</summary>
     Potato = 1 << 17,
+
+    /// <summary>Whether the plant is a caltrop.</summary>
     Caltrop = 1 << 18,
+
+    /// <summary>Whether the plant is a tangle kelp.</summary>
     TangleKelp = 1 << 19,
+
+    /// <summary>Whether the plant is magnetized.</summary>
     Magnet = 1 << 20,
+
+    /// <summary>Whether the plant is a pot, in which other plants can be planted on top of.</summary>
     Pot = 1 << 21,
+
+    /// <summary>Whether the plant takes up two spaces, such as a cob cannon.</summary>
     Double = 1 << 22,
+
+    /// <summary>Whether the plant is a spike rock.</summary>
     SpikeRock = 1 << 23,
+
+    /// <summary>Whether the plant derives from an ice-shroom.</summary>
     Ice = 1 << 24,
+
+    /// <summary>Whether the plant derives from a jalapeno.</summary>
     Fire = 1 << 25,
 }
 
+/// <summary>Contains extensions for the type <see cref="Tag"/>.</summary>
 public static class TagExtensions
 {
+    /// <summary>Indicates whether the bit is set.</summary>
+    /// <param name="tag">The tag to test.</param>
+    /// <param name="other">The bit to test.</param>
+    /// <returns>Whether the parameter <paramref name="tag"/> contains the bits of <paramref name="other"/>.</returns>
     public static bool Has(this Tag tag, Tag other) => (tag & other) == other;
 
+    /// <summary>Gets the enumeration of methods to patch.</summary>
+    /// <param name="tag">The tag to enumerate over.</param>
+    /// <returns>The enumerator responsible for getting the methods that need to be patched.</returns>
     public static Enumerator GetEnumerator(this Tag tag) => new(tag);
 
+    /// <summary>Converts the <see cref="Tag"/> into a <see cref="Plant.PlantTag"/>.</summary>
+    /// <param name="tag">The tag to convert.</param>
+    /// <returns>
+    /// The <see cref="Plant.PlantTag"/> with values set by the parameter <paramref name="tag"/>.
+    /// This conversion is lossy.
+    /// </returns>
     public static Plant.PlantTag ToPlantTag(this Tag tag) =>
         new()
         {
@@ -63,8 +122,11 @@ public static class TagExtensions
             firePlant = tag.Has(Tag.Fire),
         };
 
+    /// <summary>Represents the enumerator for <see cref="Tag"/>.</summary>
+    /// <param name="tag">The <see cref="Tag"/> to enumerate.</param>
     public struct Enumerator(Tag tag) : IEnumerator<Delegate?>
     {
+        /// <summary>The current bit.</summary>
         Tag _current = Tag.Reserved;
 
         /// <inheritdoc />
