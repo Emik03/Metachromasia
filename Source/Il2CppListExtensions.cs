d9727e75aea2e0edf2862c402ea5c01badfecee2
Emik <emik.cuber@gmail.com>
2026-04-06 22:21:53 +0200

    Add documentation for Il2CppListExtensions

--- Source/Il2CppListExtensions.cs
+++ Source/Il2CppListExtensions.cs
@@ -1,16 +1,28 @@
 // SPDX-License-Identifier: MPL-2.0
 namespace Metachromasia;
 
+/// <summary>Extension methods for <see cref="Il2CppSystem.Collections.Generic.List{T}"/>.</summary>
 public static class Il2CppListExtensions
 {
-    public static IList<T> AsIList<T>(this Il2CppSystem.Collections.Generic.List<T> l) =>
-        new List<T>(l);
+    /// <param name="list">The list to wrap.</param>
+    /// <typeparam name="T">The type of list.</typeparam>
+    extension<T>(Il2CppSystem.Collections.Generic.List<T> list)
+    {
+        /// <summary>Wraps <see cref="Il2CppSystem.Collections.Generic.List{T}"/> as <see cref="IList{T}"/>.</summary>
+        /// <returns><see cref="Il2CppSystem.Collections.Generic.List{T}"/> as <see cref="IReadOnlyList{T}"/>.</returns>
+        public IList<T> AsI() => new List<T>(list);
 
-    public static IReadOnlyList<T> AsIReadOnlyList<T>(this Il2CppSystem.Collections.Generic.List<T> l) =>
-        new List<T>(l);
+        /// <summary>
+        /// Wraps <see cref="Il2CppSystem.Collections.Generic.List{T}"/> as <see cref="IReadOnlyList{T}"/>.
+        /// </summary>
+        /// <returns><see cref="Il2CppSystem.Collections.Generic.List{T}"/> as <see cref="IReadOnlyList{T}"/>.</returns>
+        public IReadOnlyList<T> AsIReadOnly() => new List<T>(list);
+    }
 
-    sealed class List<T>(Il2CppSystem.Collections.Generic.List<T> list) : IList<T>,
-        IReadOnlyList<T>
+    /// <summary>The list wrapper for <see cref="Il2CppSystem.Collections.Generic.List{T}"/>.</summary>
+    /// <typeparam name="T">The type of list.</typeparam>
+    /// <param name="list">The list to wrap.</param>
+    sealed class List<T>(Il2CppSystem.Collections.Generic.List<T> list) : IList<T>, IReadOnlyList<T>
     {
         /// <inheritdoc />
         public bool IsReadOnly => false;
@@ -59,23 +71,32 @@ public static class Il2CppListExtensions
         /// <inheritdoc />
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 
+        /// <summary>The enumerator for <see cref="List{T}"/>.</summary>
+        /// <param name="list">The <see cref="List{T}"/> to enumerate.</param>
         // ReSharper disable once SuggestBaseTypeForParameterInConstructor
-        // ReSharper disable NullableWarningSuppressionIsUsed
         sealed class Enumerator(List<T> list) : IEnumerator<T>
         {
+            /// <summary>The current index.</summary>
             int _index;
 
+            /// <inheritdoc />
             public T Current { get; private set; } = default!;
 
+            /// <inheritdoc />
             object? IEnumerator.Current => Current;
 
+            /// <inheritdoc />
             public void Dispose() { }
 
+            /// <inheritdoc />
             public bool MoveNext() =>
                 (uint)_index >= (uint)list.Count ? OutOfBounds() : (Current = list[_index++]) is var _;
 
+            /// <inheritdoc />
             void IEnumerator.Reset() => (_index, Current) = (0, default!);
 
+            /// <summary>Sets itself to be out of bounds.</summary>
+            /// <returns>The value <see langword="false"/>.</returns>
             bool OutOfBounds() => !(((_index, Current) = (list.Count + 1, default!)) is var _);
         }
     }
